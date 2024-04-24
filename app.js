ef9e423c542bb09478f7b41afb48e8a8107ee60f
mayagoyle <mayagoyle99@gmail.com>
2024-04-23 22:09:30 -0400

    actually added files

---
+++ app.js
@@ -0,0 +1,26 @@
+
+// import OpenAI from "openai";
+
+// // const openai = new OpenAI({
+// //   organization: 'org-pxIHoXapha1iaEGbvP9V6qyc',
+// //   project: '$PROJECT_ID',
+// // });
+
+
+// const OpenAI = require("openai");
+// require("dotenv").config();
+
+
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+
+function input() {
+    var variable = document.getElementById('input_id').value;
+    document.getElementById('alert').innerHTML = 'The user input is: ' + variable;
+  }
+
+// function submitBothForms() {
+
+//  }
